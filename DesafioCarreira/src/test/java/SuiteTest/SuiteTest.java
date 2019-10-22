c4e93b945e32ad446cdf4aee6c31cb80aee5fc4c
Alan Paulo <alanpaullo@yahoo.com.br>
2019-10-22 00:12:31 -0300

    Atualização 211019

--- DesafioCarreira/src/test/java/SuiteTest/SuiteTest.java
+++ DesafioCarreira/src/test/java/SuiteTest/SuiteTest.java
@@ -1,8 +1,24 @@
 package SuiteTest;
 
+import TestCampoPersonalizado.AdicionarCampoPersonalizadoEmProjetoTest;
+import TestCampoPersonalizado.AlterarCampoPersonalizadoTest;
+import TestCampoPersonalizado.CriarCampoPersonalizadoTest;
+import TestCampoPersonalizado.RemoverCampoPersonalizadoEmProjetoTest;
+import TestCategoria.AlterarCategoriaTest;
+import TestCategoria.ApagarCategoriaTest;
+import TestCategoria.CadastrarCategoriaTest;
 import TestLogin.LoginInvalidoTest;
 import TestLogin.LoginValidoTest;
+import TestMarcador.AlterarMarcadorTest;
+import TestMarcador.CriarMarcadorTest;
+import TestMarcador.ExcluirMarcadorTest;
+import TestPerfil.AlterarPerfilTest;
+import TestPerfil.CriarPerfilTest;
+import TestProjeto.*;
 import TestTarefas.*;
+import TestToken.GerarTokenTest;
+import TestToken.RevogarTokenTest;
+import TestUsuario.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -11,11 +27,57 @@ import org.junit.runners.Suite;
 
         LoginInvalidoTest.class,
         LoginValidoTest.class,
+        AdicionarUsuarioEmProjetoTest.class,
+        AlterarUsuarioTest.class,
+        ApagarUsuarioTest.class,
+        CriarUsuarioTest.class,
+        MostrarUsuarioDesabilitadoTest.class,
+        MostrarUsuarioHabilitadoTest.class,
+        PesquisarUsuarioTest.class,
+        RedefinirSenhaUsuarioTest.class,
+        RemoverUsuarioEmProjetoTest.class,
+        RepresentarUsuarioTest.class,
+        GerarTokenTest.class,
+        RevogarTokenTest.class,
+        AdicionarFiltroTarefasTest.class,
+        AdicionarMarcadorEmTarefasTest.class,
         AlterarCategoriaEmTarefasTest.class,
+        AlterarStatusTarefasTest.class,
+        ApagarMarcadorEmTarefasTest.class,
         ApagarTarefasTest.class,
+        ClonarTarefasTest.class,
         CriarTarefasTest.class,
+        DesmarcarTarefaPegajosaTest.class,
         FecharTarefasTest.class,
-        PesquisarTarefasTest.class
+        ImprimirTarefasTest.class,
+        MarcarTarefaPegajosaTest.class,
+        MonitorarTarefasTest.class,
+        MoverTarefasTest.class,
+        //=====ERRO
+        PararDeMonitorarTarefasTest.class,
+        PesquisarTarefasTest.class,
+        RedefinirFiltroTarefasTest.class,
+        //=====ERRO
+        AlterarProjetoTest.class,
+        AlterarVisibilidadeDeProjetoPrivadoTest.class,
+        AlterarVisibilidadeDeProjetoPublicoTest.class,
+        CriarProjetoTest.class,
+        ExcluirProjetoTest.class,
+        AlterarPerfilTest.class,
+        CriarPerfilTest.class,
+        ExcluirProjetoTest.class,
+        AlterarMarcadorTest.class,
+        CriarMarcadorTest.class,
+        ExcluirMarcadorTest.class,
+        AlterarCategoriaTest.class,
+        ApagarCategoriaTest.class,
+        CadastrarCategoriaTest.class,
+        AdicionarCampoPersonalizadoEmProjetoTest.class,
+        AlterarCampoPersonalizadoTest.class,
+        CriarCampoPersonalizadoTest.class,
+        ExcluirMarcadorTest.class,
+        RemoverCampoPersonalizadoEmProjetoTest.class
+
 })
 public class SuiteTest {
 
